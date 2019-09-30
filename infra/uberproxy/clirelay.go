85d2274c625078ad76ff7c393647eb8783014496
Adam Tanana <docstuts@gmail.com>
2019-09-30 21:39:30 +1000

    it works

--- infra/uberproxy/clirelay.go
+++ infra/uberproxy/clirelay.go
@@ -8,9 +8,9 @@ import (
 )
 
 func handleCLIRelay(rsp http.ResponseWriter, req *http.Request) {
-	if req.URL.Path == "/cli-relay.py" {
+	if req.URL.Path == "/cli-relay" {
 		rsp.Header().Set("Content-Type", "text/x-python")
-		body, _ := ioutil.ReadFile(os.Args[3] + "/cli-relay.py")
+		body, _ := ioutil.ReadFile(os.Args[3] + "/cli-relay")
 		fmt.Fprint(rsp, string(body))
 	} else {
 		body, _ := ioutil.ReadFile(os.Args[3] + "/ws.html")
