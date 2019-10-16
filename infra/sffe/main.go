660131a55b1a7617c07e8725b228e671057bc889
Adam Yi <i@adamyi.com>
2019-10-16 20:26:22 +1100

    sffe: init with pre-loaded files

--- infra/sffe/main.go
+++ infra/sffe/main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	flagData := flag.String("data", "data",
 		"The location to use for the data store")
+	flagFiles := flag.String("initfiles", "/initfiles",
+		"The directory containing initial files for initiating")
 	flagAddr := flag.String("addr", ":80",
 		"The address that the HTTP server will bind")
 	flag.Parse()
@@ -20,6 +22,8 @@ func main() {
 		log.Panic(err)
 	}
 	defer ctx.Close()
+
+	context.InitFiles(ctx, *flagFiles)
 
 	log.Panic(web.ListenAndServe(*flagAddr, ctx))
 }
