357582a9a6fdf551652078119b98c1d968698515
Adam Yi <i@adamyi.com>
2019-10-19 16:30:01 +1100

    ci: optimize (#37)
    bazel loading @npm is slow... this should make it load only once

---
+++ .buildkite/ci.sh
@@ -0,0 +1,5 @@
+git-crypt unlock /g3.key
+bazel run //:gazelle -- --mode=diff || (echo \"ERROR: Bazel files out-of-date, please run \\`bazel run :gazelle\\`\" >&2; exit 1)
+bazel run //:buildifier_check || (echo \"ERROR: Bazel files not formatted, please run \\`bazel run :buildifier\\`\" >&2; exit 1)
+bazel build //infra/jsonnet:docker-compose || (echo \"ERROR: Bazel build docker-compose failed\" >&2; exit 1)
+bazel build //:all_containers || (echo \"ERROR: Bazel build all_containers failed\" >&2; exit 1)
