b63935864b4df6d4507d57d501733b37949d050b
Adam Yi <i@adamyi.com>
2019-10-21 00:35:18 +0000

    added fixes

--- infra/scoreboard/template.go
+++ infra/scoreboard/template.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, name string, data interface{}) {
-	tmpl := template.Must(template.ParseFiles(os.Args[3]+"/layouts/base.html", os.Args[3]+"/"+name))
+	tmpl := template.Must(template.ParseFiles(os.Args[2]+"/layouts/base.html", os.Args[2]+"/"+name))
 
 	err := tmpl.ExecuteTemplate(w, "base", data)
 	if err != nil {
