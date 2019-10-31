9c9df7bbababd7a14dedbf6afe6ae516afa4e101
Adam Tanana <docstuts@gmail.com>
2019-10-31 18:36:13 +1100

    Added updating

--- aws/update.sh
+++ aws/update.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 2 ]; then echo "Don't forget challenge gcr + challenge name" >&2
     exit 1
 fi
 
-docker rmi "$1"
 
 docker-compose -f ./cluster-team-docker-compose.json rm -f -s -v "$2"
+docker rmi "$1"
 docker-compose -f ./cluster-team-docker-compose.json up -d
